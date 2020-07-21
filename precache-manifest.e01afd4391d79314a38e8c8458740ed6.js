7ab12c755bbad464631b4ce3884933cbfa5fbfb0
ThomasLuu00 <Thomas.Luu00@gmail.com>
2020-07-21 14:04:07 -0400

    Updates

---
+++ precache-manifest.e01afd4391d79314a38e8c8458740ed6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1aa69ce9ef0e23140fca657c137b2f03",
+    "url": "/test/index.html"
+  },
+  {
+    "revision": "55c478b3b00d51b2d205",
+    "url": "/test/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "4c9cec222d6bffda62d8",
+    "url": "/test/static/js/2.6782a6dd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/test/static/js/2.6782a6dd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "55c478b3b00d51b2d205",
+    "url": "/test/static/js/main.4a9d5d2a.chunk.js"
+  },
+  {
+    "revision": "3e26bf9cec6d9fc0e368",
+    "url": "/test/static/js/runtime-main.e16828bb.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/test/static/media/logo.5d5d9eef.svg"
+  }
+]);
